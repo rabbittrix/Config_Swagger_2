b21d7eff5081e1bac09825debb90a8b7d967249a
Roberto de Souza <rabbittrix@hotmail.com>
2021-04-26 13:32:30 +0100

    Port config

---
+++ src/test/java/com/jrsf/swagger/confswagger/ConfswaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrsf.swagger.confswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfswaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
