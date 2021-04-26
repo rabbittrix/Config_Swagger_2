c4e62b3671fde0012faa63ba7b6ba328581aa8c7
Roberto de Souza <rabbittrix@hotmail.com>
2021-04-26 13:31:50 +0100

    Model User

---
+++ src/main/java/com/jrsf/swagger/confswagger/model/User.java
@@ -0,0 +1,15 @@
+package com.jrsf.swagger.confswagger.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+  private String userName;
+  private Long salary;
+
+}
