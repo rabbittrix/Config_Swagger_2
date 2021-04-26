651ac841735812658c6db3446e95343f3fc7248d
Roberto de Souza <rabbittrix@hotmail.com>
2021-04-26 13:32:12 +0100

    Application

---
+++ src/main/java/com/jrsf/swagger/confswagger/ConfigSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.jrsf.swagger.confswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSwaggerApplication.class, args);
+	}
+
+}
